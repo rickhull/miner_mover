ef5ef7f23504ffca6935752ab27cecca3baa57f0
Rick Hull <rick.hull@gmail.com>
2023-01-01 16:17:15 -0500

    initial commit

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Provides a shell with useful gems activated";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils}:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.bashInteractive ];
+        buildInputs = [
+          pkgs.mruby
+          (pkgs.ruby_3_1.withPackages (p: with p; [
+            rake
+            minitest
+          ]))
+        ];
+      };
+    });
+}
