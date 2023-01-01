5136f3214208ec377e37317a5e46633e21a5af82
Rick Hull <rick.hull@gmail.com>
2023-01-01 18:25:08 -0500

    make randomness optional

--- lib/miner_mover.rb
+++ lib/miner_mover.rb
@@ -17,14 +17,17 @@ class MinerMover
     n < 2 ? n : fib(n-1) + fib(n-2)
   end
 
-  def self.mine_ore(depth = 1, perform_work: false)
+  def self.mine_ore(depth = 1,
+                    perform_work: false,
+                    random_difficulty: true,
+                    random_reward: true)
     t = Time.now
     ores = Array.new(depth) { |d|
       depth_factor = 1 + d * 0.5
-      difficulty = 0.5 + rand
+      difficulty = random_reward ? (0.5 + rand) : 1
       duration = difficulty * depth_factor
       perform_work ? perform_work(duration) : perform_io(duration)
-      rand(1 + depth_factor.floor)
+      random_reward ? rand(1 + depth_factor.floor) : 1
     }
     elapsed = (Time.now - t).round(2)
     puts "#{elapsed} s #{ores.inspect}"
@@ -33,10 +36,17 @@ class MinerMover
 
   attr_reader :batch, :batch_size, :batches, :ore_moved
 
-  def initialize(batch_size, perform_work: false)
+  def initialize(batch_size, perform_work: false, random_duration: true)
     @batch_size = batch_size
     @perform_work = perform_work
+    @random_duration = random_duration
     @batch, @batches, @ore_moved = 0, 0, 0
+  end
+
+  def to_s
+    [self.object_id.to_s.rjust(8, '0'),
+     "Batch %i / %i" % [@batch, @batch_size],
+     "Moved %i (%i)" % [@batches, @ore_moved]].join(' | ')
   end
 
   def load_ore(amt)
@@ -48,8 +58,8 @@ class MinerMover
   def move_batch
     raise "unexpected batch: #{@batch}" if @batch <= 0
     amt = @batch < @batch_size ? @batch : @batch_size
-    duration = rand(amt) + 1
-    # duration = 10
+    duration = @random_duration ? (rand(amt) + 1) : amt
+
     puts "Moving #{amt} (#{duration} s) ..."
     @perform_work ? MinerMover.perform_work(duration) : MinerMover.perform_io(duration)
 
