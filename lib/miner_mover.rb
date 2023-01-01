43516c7d9641f530695c5baff0073af098a32e81
Rick Hull <rick.hull@gmail.com>
2023-01-01 18:04:04 -0500

    add perform_work vs perform_io

--- lib/miner_mover.rb
+++ lib/miner_mover.rb
@@ -1,11 +1,29 @@
 class MinerMover
-  def self.mine_ore(depth = 1)
+  def self.perform_io(duration)
+    sleep duration
+  end
+
+  def self.perform_work(duration)
+    t = Time.now
+    elapsed = 0
+    while elapsed < duration
+      fib(32)
+      elapsed = Time.now - t
+    end
+    Time.now - t
+  end
+
+  def self.fib(n)
+    n < 2 ? n : fib(n-1) + fib(n-2)
+  end
+
+  def self.mine_ore(depth = 1, perform_work: false)
     t = Time.now
     ores = Array.new(depth) { |d|
       depth_factor = 1 + d * 0.5
       difficulty = 0.5 + rand
       duration = difficulty * depth_factor
-      sleep duration
+      perform_work ? perform_work(duration) : perform_io(duration)
       rand(1 + depth_factor.floor)
     }
     elapsed = (Time.now - t).round(2)
@@ -15,8 +33,9 @@ class MinerMover
 
   attr_reader :batch, :batch_size, :batches, :ore_moved
 
-  def initialize(batch_size)
+  def initialize(batch_size, perform_work: false)
     @batch_size = batch_size
+    @perform_work = perform_work
     @batch, @batches, @ore_moved = 0, 0, 0
   end
 
@@ -32,7 +51,7 @@ class MinerMover
     duration = rand(amt) + 1
     # duration = 10
     puts "Moving #{amt} (#{duration} s) ..."
-    sleep duration # move, move, move
+    @perform_work ? MinerMover.perform_work(duration) : MinerMover.perform_io(duration)
 
     # accounting
     @ore_moved += amt
