38c5e36c672c7e68b9a1532c545efc1e0afa5b2b
Rick Hull <rick.hull@gmail.com>
2023-01-01 18:25:27 -0500

    accommodate lib changes

--- bin/ractor.rb
+++ bin/ractor.rb
@@ -12,31 +12,36 @@ CFG = {
   num_miners: 5,
   mining_depth: 4,
   miner_work: true,
+  random_difficulty: true,
+  random_reward: true,
 
-  num_movers: 3,
-  batch_size: 5,
+  num_movers: 2,
+  batch_size: 10,
   mover_work: false,
+  random_duration: true,
 }.freeze
 
 p CFG
 
 # our moving operation in a separate Ractor
 mover = Ractor.new {
-  puts "Starting the moving operation ..."
+  puts "Moving operation started ..."
   q = Thread::Queue.new
 
   movers = Array.new(CFG[:num_movers]) { |i|
     Thread.new {
       puts "Mover #{i} started ..."
-      m = MinerMover.new(CFG[:batch_size], perform_work: CFG[:mover_work])
+      m = MinerMover.new(CFG[:batch_size],
+                         perform_work: CFG[:mover_work],
+                         random_duration: CFG[:random_duration])
       loop {
         ore = q.pop
         break if ore == :quit
         m.load_ore ore
-        p m
+        puts m
       }
       m.move_batch while m.batch > 0
-      puts "QUIT: #{m.inspect}"
+      puts "QUIT: #{m}"
     }
   }
 
@@ -57,13 +62,16 @@ mover = Ractor.new {
 
 
 # Here we go!
-puts "Mining started ... \t[ctrl-c] to stop"
+puts "Mining operation started ... \t[ctrl-c] to stop"
 
 miners = Array.new(CFG[:num_miners]) { |i|
   Thread.new {
     puts "Miner #{i} started ..."
     while !stop_mining
-      ore = MinerMover.mine_ore(CFG[:mining_depth], perform_work: CFG[:miner_work])
+      ore = MinerMover.mine_ore(CFG[:mining_depth],
+                                perform_work: CFG[:miner_work],
+                                random_difficulty: CFG[:random_difficulty],
+                                random_reward: CFG[:random_reward])
       mover.send ore if ore > 0
       break if stop_mining
     end
