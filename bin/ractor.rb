1c88a918ec284413e93d7363dbe67a4fa4403b74
Rick Hull <rick.hull@gmail.com>
2023-01-01 21:42:35 -0500

    improve comments

--- bin/ractor.rb
+++ bin/ractor.rb
@@ -30,7 +30,7 @@ puts
 puts CFG.to_a.map { |(k, v)| format("%s: %s", k, v) }
 puts
 
-# our moving operation in a separate Ractor
+# our moving operation executes in its own Ractor
 mover = Ractor.new(t) { |t|
   t.stamp! "MOVE Moving operation started"
   q = Thread::Queue.new
@@ -42,6 +42,7 @@ mover = Ractor.new(t) { |t|
                          random_duration: CFG[:random_duration])
       t.stamp! "MOVE Mover #{i} started (#{m.object_id})"
       loop {
+        # a mover picks up ore from the queue
         ore = q.pop
         break if ore == :quit
         m.load_ore ore
@@ -53,36 +54,44 @@ mover = Ractor.new(t) { |t|
     }
   }
 
-  # main thread feeds the queue with ore
-  # and tells the workers when to quit
+  # Miners feed this Ractor with ore
+  # Pass the ore into a queue for the movers
+  # When the miners say to quit, tell the movers to quit
   t.stamp! "WAIT Waiting for ore ..."
   loop {
+    # when the Ractor gets ore, push it into the queue
     ore = Ractor.recv
     break if ore == :quit
     t.stamp! "RECV #{ore} ore"
     q.push ore
   }
+
+  # tell all the movers to quit and gather their results
   CFG[:num_movers].times { q.push :quit }
   movers.map { |thr| thr.value.ore_moved }.sum
 }
 
-# Here we go!
+# our mining operation executes in the main Ractor, here
 t.stamp! "MINE Mining operation started  [ctrl-c] to stop"
 miners = Array.new(CFG[:num_miners]) { |i|
   # spread out miners if uniform difficulty
   sleep 0.5 if !CFG[:random_difficulty] and i > 0
+
   Thread.new {
     t.stamp! "MINE Miner #{i} started"
     ore_mined = 0
+
+    # miners wait for the SIGINT signal to quit
     while !stop_mining
       ore = MinerMover.mine_ore(CFG[:mining_depth],
                                 perform_work: CFG[:miner_work],
                                 random_difficulty: CFG[:random_difficulty],
                                 random_reward: CFG[:random_reward])
+      # send any ore mined to the mover Ractor
       mover.send ore if ore > 0
       ore_mined += ore
-      break if stop_mining
     end
+
     t.stamp! "MINE Miner #{i} stopped after mining #{ore_mined} ore"
     ore_mined
   }
