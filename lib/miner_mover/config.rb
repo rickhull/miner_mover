f1e92c6572bf5b96330c74b1bbedae39025aeeb7
Rick Hull <rick.hull@gmail.com>
2023-01-08 17:58:01 -0500

    stop deleting; add self.rewrite

--- lib/miner_mover/config.rb
+++ lib/miner_mover/config.rb
@@ -47,26 +47,36 @@ module MinerMover
     end
 
     # return a hash with :miner, :mover, :main keys
-    def self.process(file = nil)
-      file ||= self.recent
-      cfg = DotCfg.new(file)
+    def self.process(file = nil, cfg: nil)
+      if cfg.nil?
+        file ||= self.recent
+        cfg = DotCfg.new(file)
+      end
 
-      if cfg['miner'] or cfg['mover']
+      if cfg['miner'] or cfg['mover'] or cfg['main']
         # convert string keys to symbols
-        miner = (cfg.delete('miner') || {}).transform_keys { |k| k.to_sym }
-        mover = (cfg.delete('mover') || {}).transform_keys { |k| k.to_sym }
-        main  = (cfg.delete('main')  || {}).transform_keys { |k| k.to_sym }
-      elsif cfg[:miner] or cfg[:mover]
+        miner = (cfg['miner'] || {}).transform_keys { |k| k.to_sym }
+        mover = (cfg['mover'] || {}).transform_keys { |k| k.to_sym }
+        main  = (cfg['main']  || {}).transform_keys { |k| k.to_sym }
+      elsif cfg[:miner] or cfg[:mover] or cfg[:main]
         # assume all keys are symbols
-        miner = cfg.delete(:miner) || {}
-        mover = cfg.delete(:mover) || {}
-        main  = cfg.delete(:main)  || {}
+        miner = cfg[:miner] || {}
+        mover = cfg[:mover] || {}
+        main  = cfg[:main]  || {}
       else
-        raise "couldn't find miner or mover in #{file}"
+        raise "couldn't find miner, mover, or main in #{file}"
       end
       { miner: DEFAULT[:miner].merge(miner),
         mover: DEFAULT[:mover].merge(mover),
         main:  DEFAULT[:main].merge(main) }
+    end
+
+    # rewrites the dotcfg file, filling in any defaults, and symbols for keys
+    def self.rewrite(file)
+      cfg = DotCfg.new(file)
+      hsh = self.process(cfg: cfg)
+      hsh.each { |k, v| cfg[k] = v }
+      cfg.save
     end
   end
 end
